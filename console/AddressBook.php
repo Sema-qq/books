a64be21ab830b5cca0c1b0a6654c3f11b11c65ba
sema <8sd47w82>
2018-01-23 17:54:24 +0500

    Добавлены валидатор и обработчик

--- console/AddressBook.php
+++ console/AddressBook.php
@@ -46,32 +46,12 @@ class AddressBook extends Table
 
     /**
      * Редактирование одного контакта
+     * @param object $contact
+     * @return bool
      */
-    public function edit()
+    public function edit($contact)
     {
-        echo "Enter number contact: ";
-        $contact = $this->findOne($this->validateId(rtrim(fgets(STDIN))));
-
-        if (!empty($contact)){
-            echo "ID:       $contact->id\n";
-            echo "F.I.O.:   $contact->fio.\n";
-            echo "Phone:    $contact->phone.\n";
-            echo "Enter new F.I.O. for contact: ";
-            $fio = rtrim(fgets(STDIN));
-            echo "Enter new Phone for contact: ";
-            $phone = rtrim(fgets(STDIN));
-            $contact->fio = !empty($fio) ? $this->validateFio($fio) : $contact->fio;
-            $contact->phone = !empty($phone) ? $this->validatePhone($phone) : $contact->phone;
-            if ($this->update($contact)){
-                echo "Contact $contact->id successfully edited!\n";
-                echo "\n";
-            } else {
-                echo "Contact update failure!\n";
-            }
-        } else {
-            echo "Contact not found!\n";
-            echo "\n";
-        }
+        return $this->update($contact);
     }
 
     /**
@@ -79,19 +59,7 @@ class AddressBook extends Table
      */
     public function showAll()
     {
-        $contacts = $this->getAll();
-
-        if (!empty($contacts)){
-            foreach ($contacts as $contact){
-                echo "ID:       $contact->id\n";
-                echo "F.I.O.:   $contact->fio.\n";
-                echo "Phone:    $contact->phone.\n";
-                echo "\n";
-            }
-        } else {
-            echo "Contacts not found!\n";
-            echo "\n";
-        }
+        return $this->getAll();
     }
 
     /**
@@ -102,56 +70,5 @@ class AddressBook extends Table
     public function delete($id)
     {
         return !empty($id) ? $this->destroy($id) : false;
-    }
-
-    /**
-     * Валидация телефон
-     * @param string $phone
-     * @return string
-     */
-    private function validatePhone($phone)
-    {
-        while (true){
-            //только цифры и не меньше 5 цифр
-            if (preg_match('([^0-9]+)', $phone)){
-                echo "Must be only numbers! Please try again enter phone: ";
-                $phone = rtrim(fgets(STDIN));
-            } elseif (strlen($phone) < 5){
-                echo "Length 5 characters! Please try again enter phone: ";
-                $phone = rtrim(fgets(STDIN));
-            } else { return $phone; }
-        }
-    }
-
-    /**
-     * Валидация имени контакта
-     * @param string $fio
-     * @return string
-     */
-    private function validateFio($fio)
-    {
-        while (true){
-            //только буквы и цифры
-            if (strlen($fio) < 3){
-                echo "Length 3 characters! Please try again enter F.I.O.: ";
-                $fio = rtrim(fgets(STDIN));
-            } else { return $fio; }
-        }
-    }
-
-    /**
-     * Валидация номера контакта
-     * @param string $id
-     * @return string
-     */
-    private function validateId($id)
-    {
-        while (true){
-            //только цифры
-            if (preg_match('([^0-9]+)', $id)){
-                echo "Must be only numbers! Please try again enter number contact: ";
-                $id = rtrim(fgets(STDIN));
-            } else { return $id; }
-        }
     }
 }
